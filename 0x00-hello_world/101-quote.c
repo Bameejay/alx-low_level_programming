d47b595e33c709b495d5995867fc02ad5ed078c3
JayJay0830 <johnsonkayode01@gmail.com>
2022-03-10 19:16:34 -0800

    My 8th task accomplished

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My Unix is basically a simple OS
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("and that peice of art is useful" - Dora korpar, 2015-10-19,\n");
+return (1);
