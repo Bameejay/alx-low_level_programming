4889256a14a2c19ab2939b515af0cb3d821ab4ae
JayJay0830 <johnsonkayode01@gmail.com>
2022-03-16 18:17:02 -0700

    Task 12 completed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,68 +1,50 @@
 #include "main.h"
-/**
- * print_nums - prints out 3, 2, and 1 digit numbers with spaces
- * @num: this is the multiplied number
- * Return: void just pring to console.
- */
-void print_nums(int num)
-{
-int x, y, z;
 
-if (num > 99)
-{
-x = num / 100 + '0';
-y = num / 10 % 10  + '0';
-z = num % 10 + '0';
-_putchar(' ');
-_putchar(x);
-_putchar(y);
-_putchar(z);
-}
-else if (num > 9)
-{
-x = num / 10 + '0';
-y = num % 10 + '0';
-_putchar(' ');
-_putchar(' ');
-_putchar(x);
-_putchar(y);
-}
-else
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(num + '0');
-}
-}
 /**
- * print_times_table - print the times table depending on input
- * @n: input that will dicate size of times table.
- *
- * print_nums
- * Return: void, print out to console.
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
+
 void print_times_table(int n)
 {
-int i, j, num;
+	int x = 0, y, z;
 
-if (n >= 0 && n < 16)
-{
-for (i = 0; i <= n; ++i)
-{
-for (j = 0; j <= n; ++j)
-{
-num = i * j;
-if (j == 0)
-_putchar('0');
-else
-print_nums(num);
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
 
-if (j < n)
-_putchar(',');
-else
-_putchar('\n');
-}
-}
-}
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
 }
