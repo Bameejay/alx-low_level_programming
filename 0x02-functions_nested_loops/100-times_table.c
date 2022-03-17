225290518fdde4daf83e4afa56c79dadef8a9c35
JayJay0830 <johnsonkayode01@gmail.com>
2022-03-16 18:59:21 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,54 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_times_table - prints times table
- * @n : times table to use
- * Return:void
+ *print_times_table - n times table.
+ *@n: the number defines the table.
+ *
+ *Return: no return value
  */
-
 void print_times_table(int n)
-
 {
-
-int x = 0, rep, y;
-
-if (n < 0 || n > 15)
-	return;
-
-while (x <= n)
-{
-	for (y = 0; y <= n; y++)
+	int i, j, k,product;
+	j = n + 1;
+	for (i = 0; i < j; i++)
 	{
-		rep = x * y;
-		if (y == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+		for (k = 0; k < j; k++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
+			if (n > 15)
+			{
+				break;
+			}
+			else
+			{
+				product = i * k;
+				if (k == 0)
+		       		{
+					_putchar('0' + product);
+				}
+				else if (product >= 10)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' +  (product % 10));
+				}
+				else if (product >= 100)
+				{
+	       
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				if (k != n)
+				{
+					_putchar(',');
+				}
+			}
 		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (y < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	x++;
-}
 }
